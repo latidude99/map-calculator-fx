fc51ba960af0ba7eb1284d08886f1454a63411a5
Piotr <latidude99@gmail.com>
2019-05-05 21:23:27 +0100

    Add Maven support
    Add folder for images and icons in resources.
    Configure Maven for JUnit5 tests.
    Configure Maven to automatically build an executable .jar
    and a wrapper .exe (Launch4J).
    Pack it in a ZIP format including required help images from resources.
    Test all of the above - passed.

---
+++ src/test/java/MapConverterTests.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/*
+ *
+ */
+
+public class MapConverterTests {
+
+
+    @BeforeEach
+    public void init(){
+
+    }
+
+    @AfterEach
+    public void tearDown(){
+
+    }
+
+    /*
+     *
+     */
+    @Test
+    @DisplayName("")
+    public void test1(){
+
+    }
+
+
+
+
+
+}
